6edd721e0bd72306d27da19a15bb4cc03fe4f650
Ect07 <ect07aha@hotmail.com>
2024-02-13 18:09:06 +0800

    Add project files.

---
+++ ScreenSaver/SettingsForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FloweraveSCR
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.previewButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(234)))));
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.textBox.Location = new System.Drawing.Point(39, 163);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(455, 27);
+            this.textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.label1.Location = new System.Drawing.Point(32, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Flower Avenue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.label3.Location = new System.Drawing.Point(34, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "时间间隔 (ms)";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.okButton.Location = new System.Drawing.Point(361, 403);
+            this.okButton.Margin = new System.Windows.Forms.Padding(4);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(100, 37);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "保存";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.CausesValidation = false;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.cancelButton.Location = new System.Drawing.Point(469, 403);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(100, 37);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "取消";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // previewButton
+            // 
+            this.previewButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.previewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previewButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previewButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(153)))));
+            this.previewButton.Location = new System.Drawing.Point(253, 403);
+            this.previewButton.Margin = new System.Windows.Forms.Padding(4);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(100, 37);
+            this.previewButton.TabIndex = 6;
+            this.previewButton.Text = "预览";
+            this.previewButton.UseVisualStyleBackColor = true;
+            this.previewButton.Click += new System.EventHandler(this.previewButton_Click);
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.previewButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "SettingsForm";
+            this.Text = "FloweraveSCR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button previewButton;
+    }
+}
---
+++ ScreenSaver/SettingsForm.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * SettingsForm.cs
+ * By Frank McCown
+ * Summer 2010
+ * 
+ * Feel free to modify this code.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Security.Permissions;
+
+namespace FloweraveSCR
+{
+    public partial class SettingsForm : Form
+    {
+        public SettingsForm()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        /// <summary>
+        /// Load display text from the Registry
+        /// </summary>
+        private void LoadSettings()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\FloweraveSCR");
+            if (key == null)
+                textBox.Text = "5000";
+            else
+                textBox.Text = ((int)key.GetValue("Interval")).ToString();
+        }
+
+        /// <summary>
+        /// Save text into the Registry.
+        /// </summary>
+        private void SaveSettings()
+        {
+            // Create or get existing subkey
+            RegistryKey key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\FloweraveSCR");
+
+            int intervalInt = 0;
+
+            if (Int32.TryParse(textBox.Text, out intervalInt))
+                key.SetValue("Interval", intervalInt);
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void previewButton_Click(object sender, EventArgs e)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                SaveSettings();
+                ScreenSaverForm screensaver = new ScreenSaverForm(screen.Bounds);
+                screensaver.Show();
+            }
+        }
+    }
+}
